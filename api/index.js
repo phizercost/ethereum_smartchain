4106d61e30e0b6063939955fd72042d9ed0ee294
phizercost <phizer.development@gmail.com>
2020-02-13 10:52:58 +0200

    Broadcast blocks to peers

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const Blockchain = require("../blockchain");
 const Block = require("../blockchain/block");
+const PubSub = require("./pubsub");
 
 const app = express();
 const blockchain = new Blockchain();
+const pubsub = new PubSub({blockchain});
 
 app.get("/blockchain", (req, res, next) => {
   const { chain } = blockchain;
@@ -14,10 +16,11 @@ app.get("/blockchain", (req, res, next) => {
 app.get("/blockchain/mine", (req, res, next) => {
   const lastBlock = blockchain.chain[blockchain.chain.length - 1];
   const block = Block.mineBlock({ lastBlock });
-  block.blockHeaders.parentHash = 'blabla';
+
   blockchain
     .addBlock({ block })
     .then(() => {
+      pubsub.broadcastBlock(block);
       res.json({ block });
     })
     .catch(next);
@@ -29,5 +32,8 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
 
-const PORT = 3000;
-app.listen(3000, () => console.log(`Listening at PORT:${PORT}`));
+const PORT = process.argv.includes("--peer")
+  ? Math.floor(2000 + Math.random() * 1000)
+  : 3000;
+
+app.listen(PORT, () => console.log(`Listening at PORT:${PORT}`));
